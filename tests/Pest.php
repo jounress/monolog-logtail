db1087cbdd963319b819b3dcb00443ff51a02f9c
Jorge P. Hernandez Lalcebo <lalcebo2003@gmail.com>
2024-11-29 23:57:53 -0500

    chore: downgrade pestphp

--- tests/Pest.php
+++ tests/Pest.php
@@ -13,9 +13,7 @@ declare(strict_types=1);
 |
 */
 
-pest()
-    ->extend(Tests\TestCase::class)
-    ->in('Feature');
+uses(Tests\TestCase::class)->in('Feature');
 
 /*
 |--------------------------------------------------------------------------
